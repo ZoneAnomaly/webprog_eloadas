ac844aa0b460cbd45cb5394c4a9884663e008a81
Wratek <wratek@gmail.com>
2026-04-14 15:32:31 +0200

    index alap + db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = "localhost"; 
+$db = "adatbazis969"; 
+$user = "adatbazis969";  
+$pass = "getdata9161"; 
+
+try {
+    
+    $pdo = new PDO("mysql:host='$host';dbname=$db;charset=UTF8", $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
